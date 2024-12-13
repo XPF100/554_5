485cf2647b8520d09591397f61708eafb494c9b1
Alex Edwards <xpf100@me.com>
2024-12-13 16:56:04 +0000

    added to git ignore

--- test.R
+++ test.R
@@ -2,6 +2,6 @@
 #https://happygitwithr.com/https-pat.html
 #https://sites.northwestern.edu/researchcomputing/resources/using-git-and-github-with-r-rstudio/
 
-usethis::use_git_config(user.name="xpf100", user.email="xpf100@me.com")
+usethis::use_git_config(user.name="usr", user.email="usr")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
